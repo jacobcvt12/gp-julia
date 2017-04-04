29974afff425b0570879c897df0ff6c03cb33ff6
Jacob Carey <jacobcvt12@gmail.com>
2017-04-03 22:18:02 -0400

    Clean up draw from GP prior

--- GPML/chap2.jl
+++ GPML/chap2.jl
@@ -1,23 +1,29 @@
 using Distributions, Distances
 
-# squared exponential covariance function
-function K(X_p, X_q)
-    return 1
-end
-
 # set seed
 srand(1)
 
+# sample from gaussian process
+
+# covariate
 x_star = -5:0.1:5
+
+# squared exponential covariance function
 K_psd = exp(-0.5 * pairwise(SqEuclidean(), x_star'))
-K_pd = copy(K);
+
+# correct issue with K being singular
+K_pd = copy(K_psd)
+
 for i in 1:length(x_star)
     K_pd[i, i] = K_pd[i, i] + 0.001
 end
-det(K_pd)
-det(K_psd)
-f_star = rand(MvNormal(K_pd), 1)
 
-using Gadfly
+f_star = rand(MvNormal(K_pd), 3)
 
-plot(x=x_star, y=f_star[:, ], Geom.point, Geom.line)
+# now plot draws from GP prior
+using Gadfly, DataFrames
+gp_draws = DataFrame(f_star)
+gp_draws[:x_star] = x_star
+gp_draws = stack(gp_draws, [:x1, :x2, :x3], [:x_star])
+
+plot(gp_draws, x="x_star", y="value", Geom.point)
